b22851bfce13ff809d0828c4564f23cacbe6f2a9
remtori <lqvu99@gmail.com>
2020-10-15 22:48:35 +0700

    Reme.h expose more class/method

--- Reme/Reme.h
+++ Reme/Reme.h
@@ -1,15 +1,26 @@
 #pragma once
 
 #include <Reme/Core/Application.h>
+#include <Reme/Core/Asset.h>
 #include <Reme/Core/Core.h>
+
 #include <Reme/Events/Input.h>
+
 #include <Reme/Renderer/Color.h>
+#include <Reme/Renderer/Image.h>
+
 #include <Reme/Renderer/OrthographicCamera.h>
 #include <Reme/Renderer/PerspectiveCamera.h>
+
 #include <Reme/Renderer/Renderer2D.h>
+#include <Reme/Renderer/RendererAPI.h>
 
 using Reme::Application;
 using Reme::Color;
+using Reme::Image;
+using Reme::make;
+using Reme::make_asset;
+using Reme::make_own;
 using Reme::OrthographicCamera;
 using Reme::OwnPtr;
 using Reme::PerspectiveCamera;
