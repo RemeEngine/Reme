95d4a83057cef9460006c5869673317502a53d32
remtori <lqvu99@gmail.com>
2020-10-29 11:06:02 +0700

    Reme/Core/Asset: Out of line defination + thread safe add(), remove() asset

--- Reme/Core/Asset.cpp
+++ Reme/Core/Asset.cpp
@@ -1,13 +1,57 @@
 #include <Reme/Core/Asset.h>
+#include <atomic>
+#include <mutex>
 
 namespace Reme {
 
-AssetUID AssetManager::s_uid_count = 1;
-std::unordered_map<AssetUID, WeakPtr<Asset>> AssetManager::s_asset_map;
+static std::atomic<AssetUID> s_uid_count { 1 };
+
+static std::mutex s_asset_mutex;
+static std::unordered_map<AssetUID, WeakPtr<Asset>> s_asset_map;
 
 AssetUID AssetManager::next_uid()
 {
     return s_uid_count++;
+}
+
+RefPtr<Asset> AssetManager::get_asset(AssetUID uid)
+{
+    std::scoped_lock lock(s_asset_mutex);
+
+    auto it = s_asset_map.find(uid);
+    if (it == s_asset_map.end())
+        return nullptr;
+
+    auto asset = it->second;
+    if (asset.expired()) {
+        s_asset_map.erase(it);
+        return nullptr;
+    }
+
+    return asset.lock();
+}
+
+void AssetManager::add(const Asset* ptr)
+{
+    std::scoped_lock lock(s_asset_mutex);
+    s_asset_map.emplace(ptr->uid(), const_cast<Asset*>(ptr)->weak_from_this());
+}
+
+void AssetManager::remove(AssetUID uid)
+{
+    const std::lock_guard<std::mutex> lock(s_asset_mutex);
+
+    auto it = s_asset_map.find(uid);
+    if (it == s_asset_map.end()) {
+        CORE_LOG_ERROR("AssetManager: Tried to remove asset with unknown uid ({})", uid);
+        return;
+    }
+
+#ifdef REME_TRACE_ASSET_LIFETIME
+    CORE_LOG_TRACE("Removed ({})", uid);
+#endif
+
+    s_asset_map.erase(it);
 }
 
 } // namespace Reme
--- Reme/Core/Asset.h
+++ Reme/Core/Asset.h
@@ -12,21 +12,7 @@ class Asset;
 class AssetManager {
 public:
     static AssetUID next_uid();
-
-    static RefPtr<Asset> get_asset(AssetUID uid)
-    {
-        auto it = s_asset_map.find(uid);
-        if (it == s_asset_map.end())
-            return nullptr;
-
-        auto asset = it->second;
-        if (asset.expired()) {
-            s_asset_map.erase(it);
-            return nullptr;
-        }
-
-        return asset.lock();
-    }
+    static RefPtr<Asset> get_asset(AssetUID uid);
 
     template<typename T>
     static RefPtr<T> get(AssetUID uid)
@@ -41,8 +27,8 @@ public:
 private:
     friend Asset;
 
-    static AssetUID s_uid_count;
-    static std::unordered_map<AssetUID, WeakPtr<Asset>> s_asset_map;
+    static void add(const Asset*);
+    static void remove(AssetUID uid);
 };
 
 class Asset : public RefCounted<Asset> {
@@ -50,8 +36,8 @@ public:
     template<typename T, typename... Args>
     ALWAYS_INLINE static RefPtr<T> make(Args&&... args)
     {
-        RefPtr<T> ptr = std::make_shared<T>(std::forward<Args>(args)...);
-        AssetManager::s_asset_map[ptr->uid()] = ptr->weak_from_this();
+        RefPtr<T> ptr = std::make_shared<T>(std::forward<Args>(args)...);        
+        AssetManager::add(ptr.get());
 
 #ifdef REME_TRACE_ASSET_LIFETIME
         CORE_LOG_TRACE("Created ({}) {}", ptr->uid(), ptr->class_name());
@@ -62,17 +48,13 @@ public:
 
     virtual ~Asset()
     {
-#ifdef REME_TRACE_ASSET_LIFETIME
-        CORE_LOG_TRACE("Removed ({})", uid());
-#endif
+        AssetManager::remove(uid());
     };
 
     ALWAYS_INLINE AssetUID uid() const { return m_uid; }
     virtual const char* class_name() const = 0;
 
 protected:
-    friend AssetManager;
-
     Asset()
         : m_uid(AssetManager::next_uid())
     {
